09d7fe2d3043df3a75e81e07e4d57c7fbf959c76
Karl Johan Ryberg <karl.ryberg@ne.se>
2014-11-17 12:33:09 +0100

    fgh

--- Experiments/LMS-scroll/resources/scripts/NE.Initiation.js
+++ Experiments/LMS-scroll/resources/scripts/NE.Initiation.js
@@ -57,9 +57,7 @@ $(window).load(function () {
         NE.EventHandlers.ChapterLabelClick($(this));
     });
 
-    $('#NE-chapter-label-xs').on('click', function () {
-        NE.EventHandlers.ChapterLabelXsClick($(this));
-    });
+
 
     $(document).on('keyup', NE.EventHandlers.KeyUp);
 
