a4db24bd90ee2ad8c7f772899fa55b81d04a7161
Karl Johan Ryberg <karl.ryberg@ne.se>
2016-01-20 15:18:15 +0100

    asdasd

---
+++ Experiments/JavaScript-2h/LazyCanvas.js
@@ -0,0 +1,24 @@
+﻿var LazyCanvas = function () {
+    this.Canvas = null;
+    this.Width = 0;
+    this.Height = 0;
+    this.Context = null;
+}
+
+LazyCanvas.prototype.Init = function (element) {
+
+    var canvasElement = document.createElement('canvas');
+    canvasElement.setAttribute('width', element.scrollWidth.toString());
+    canvasElement.setAttribute('height', element.scrollHeight.toString());
+
+    var ctx = canvasElement.getContext('2d');
+
+    this.Canvas = canvasElement;
+    this.Context = ctx;
+    this.Width= ctx.canvas.width;
+    this.Height = ctx.canvas.height;
+
+    element.innerHTML = '';
+    element.appendChild(canvasElement);
+
+};
