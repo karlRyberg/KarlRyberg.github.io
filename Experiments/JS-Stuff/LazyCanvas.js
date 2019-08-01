dec50aec162a3b4cad0be78be5cf035d2d1bea77
Karl-Johan Ryberg <s24112@semcon.se>
2019-08-01 08:30:53 +0200

    DIray first day

---
+++ Experiments/JS-Stuff/LazyCanvas.js
@@ -0,0 +1,24 @@
+﻿var LazyCanvas = function () {
+    this.Canvas = null;
+    this.Width = 0;
+    this.Height = 0;
+    this.Context = null;
+    return this;    
+}
+
+LazyCanvas.prototype.Init = function (element) {
+
+    var canvasElement = document.createElement('canvas');
+    canvasElement.setAttribute('width', element.offsetWidth.toString());
+    canvasElement.setAttribute('height', element.offsetHeight.toString());
+
+    var ctx = canvasElement.getContext('2d');
+
+    this.Canvas = canvasElement;
+    this.Context = ctx;
+    this.Width= ctx.canvas.width;
+    this.Height = ctx.canvas.height;
+
+    element.innerHTML = '';
+    element.appendChild(canvasElement);
+};
